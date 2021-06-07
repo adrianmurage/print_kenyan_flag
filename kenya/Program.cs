3b1f95bf8b5b94f4de520fb8d5ed86c37393ebdc
Adrian Murage <adrianmurage21@gmail.com>
2021-06-07 21:33:21 +0300

    add code to console output the kenyan flag

---
+++ kenya/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace kenya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Background color
+            Console.BackgroundColor = ConsoleColor.Blue;
+            
+            // First line
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+            // Second line
+            Console.Write("xxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("xxxxxxxxxxxxx");
+
+            // Third line 
+            Console.Write("xxxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("xxxxxxxxxxxxxx");
+
+            // Fourth line
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("xxxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("xxxxxxxxxxxxxx");
+
+            // Fifth line
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxx");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("xxxxxxxxxxxxx");
+
+            // Sixth line
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("xxxxxxxxxxxx");
+
+            //Seventh line
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxx");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("xxxxxxxxxxxxx");
+
+            // Eigth line
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("xxxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxxxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("xxxxxxxxxxxxxx");
+
+            // Ninth line
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("xxxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("xxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("xxxxxxxxxxxxxx");
+
+            // Tenth line
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("xxxxxxxxxxxxx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("xx");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("x");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("xxxxxxxxxxxxx");
+
+            // Eleventh line
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+            Console.ResetColor();
+        }
+    }
+}
